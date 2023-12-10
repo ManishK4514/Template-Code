a76e962d9133b4bd5c0fbf316e37e40be138075b
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-12-10 13:34:13 +0530

    Create PowerFunction.java

---
+++ PowerFunction.java
@@ -0,0 +1,11 @@
+private long power(long base, int exponent, int mod) {
+        long result = 1;
+        while (exponent > 0) {
+            if (exponent % 2 == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exponent /= 2;
+        }
+        return result;
+    }
